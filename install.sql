5dd6eeb8f80fe99fe2ef3cb27f933990c51d636b
henrymops89 <henry.mops89@gmail.com>
2026-02-27 16:50:12 +0100

    Update install.sql
    Fahrzeuge werden nun wirklich in der Datenbank gespeichert

--- install.sql
+++ install.sql
@@ -5,11 +5,10 @@
 
 CREATE TABLE IF NOT EXISTS `hm_handling_profiles` (
     `id`          INT          NOT NULL AUTO_INCREMENT,
-    `citizenid`   VARCHAR(50)  NOT NULL,
+    `vehicle`     VARCHAR(64)  NOT NULL,
     `name`        VARCHAR(64)  NOT NULL,
-    `vehicle`     VARCHAR(64)  NOT NULL DEFAULT '',
     `data`        LONGTEXT     NOT NULL,
     `created_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    KEY `idx_citizenid` (`citizenid`)
+    KEY `idx_vehicle` (`vehicle`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
